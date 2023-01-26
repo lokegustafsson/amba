f58a405ca9ae1745d3e240fe4ebbe55cf505749c
Samuel Kyletoft <samuel@kyletoft.se>
2023-01-26 15:41:51 +0100

    Rust: stable compiler, nightly clippy + rustfmt

--- flake.nix
+++ flake.nix
@@ -42,7 +42,8 @@
             packages = let p = pkgs;
             in [
               cargo2nix.outputs.packages.${system}.cargo2nix
-              p.rust-bin.stable.latest.clippy
+              p.rust-bin.nightly.latest.clippy
+              p.rust-bin.nightly.latest.rustfmt
               p.rust-bin.stable.latest.default
             ] ++ builtins.attrValues rust.packages;
           };
