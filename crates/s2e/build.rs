acf603d7b914a748cf250405c258f0e2e7992216
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-11 01:34:33 +0100

    Replace local copy of s2e with Nix version

--- crates/s2e/build.rs
+++ crates/s2e/build.rs
@@ -1,21 +1,27 @@
 use std::path::PathBuf;
 
+fn s2e_subpath(s: &str) -> PathBuf {
+	let mut s2e = PathBuf::from(env!("S2E_PATH"));
+	s2e.push(s);
+	s2e
+}
+
 fn main() -> miette::Result<()> {
 	// s2e subprojects
-	let klee = PathBuf::from("../../s2e/klee/include");
-	let coroutine = PathBuf::from("../../s2e/libcoroutine/include");
-	let cpu = PathBuf::from("../../s2e/libcpu/include");
-	let fsigcxx = PathBuf::from("../../s2e/libfsigc++/include");
-	let q = PathBuf::from("../../s2e/libq/include");
-	let s2e = PathBuf::from("../../s2e/libs2e/include");
-	let s2ecore = PathBuf::from("../../s2e/libs2ecore/include");
-	let s2eplugins = PathBuf::from("../../s2e/libs2eplugins/include");
-	let tcg = PathBuf::from("../../s2e/libtcg/include");
-	let vmi = PathBuf::from("../../s2e/libvmi/include");
+	let klee = s2e_subpath("klee/include");
+	let coroutine = s2e_subpath("libcoroutine/include");
+	let cpu = s2e_subpath("libcpu/include");
+	let fsigcxx = s2e_subpath("libfsigc++/include");
+	let q = s2e_subpath("libq/include");
+	let s2e = s2e_subpath("libs2e/include");
+	let s2ecore = s2e_subpath("libs2ecore/include");
+	let s2eplugins = s2e_subpath("libs2eplugins/include");
+	let tcg = s2e_subpath("libtcg/include");
+	let vmi = s2e_subpath("libvmi/include");
 
 	// There are 5 files with the same name in s2e. 3 x86 and 2 x86_64
 	// versions. I'm using the first x86_64 version.
-	let config_host = PathBuf::from("../../s2e/tools/lib/X8664BitcodeLibrary");
+	let config_host = s2e_subpath("tools/lib/X8664BitcodeLibrary");
 
 	// Dependencies
 	let glibc = PathBuf::from(env!("GLIBC_PATH"));
