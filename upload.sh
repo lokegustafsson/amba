42377bbeb82a9a99f329e045a956cfcb992e0685
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-27 23:54:49 +0100

    Move upload script into makefile

--- upload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-nix build . --accept-flake-config
-doas nix store sign --key-file ./eurydice-private-key --recursive ./result-bin
-nix store verify --trusted-public-keys $(nix key convert-secret-to-public < ./eurydice-private-key) .
-NIX_SSHOPTS="-p1234" nix copy . --to ssh://nix.u3836.se
