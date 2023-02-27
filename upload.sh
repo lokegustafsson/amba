8c01f5209cf07d91c265a0c6e7d879ef30f575f7
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-27 23:53:26 +0100

    upload script: eurydice → nix.u3836.se + $NIX_SSHOPTS

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ set -e
 nix build . --accept-flake-config
 doas nix store sign --key-file ./eurydice-private-key --recursive ./result-bin
 nix store verify --trusted-public-keys $(nix key convert-secret-to-public < ./eurydice-private-key) .
-nix copy . --to ssh://eurydice
+NIX_SSHOPTS="-p1234" nix copy . --to ssh://nix.u3836.se
