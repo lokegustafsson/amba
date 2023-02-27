4069214fa8215c3733ff718f3f273522cd2172e1
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-27 23:52:53 +0100

    Add accept flake config to upload script

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-nix build .
+nix build . --accept-flake-config
 doas nix store sign --key-file ./eurydice-private-key --recursive ./result-bin
 nix store verify --trusted-public-keys $(nix key convert-secret-to-public < ./eurydice-private-key) .
 nix copy . --to ssh://eurydice
