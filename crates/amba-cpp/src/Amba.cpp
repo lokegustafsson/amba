1f221a0e2b3101dac6623a34cdc156d7c18fb1e6
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-02 00:10:47 +0100

    Outline plan for implementation of overrun

--- crates/amba-cpp/src/Amba.cpp
+++ crates/amba-cpp/src/Amba.cpp
@@ -1,15 +1,15 @@
-#include "Numbers.h"
-
 #include <s2e/ConfigFile.h>
 #include <s2e/S2E.h>
 #include <s2e/Utils.h>
 
 #include "Amba.h"
+#include "Numbers.h"
 
 namespace s2e {
 namespace plugins {
 
 S2E_DEFINE_PLUGIN(Amba, "Amba S2E plugin", "", );
+#define SUBSCRIBE(fn) signal->connect(sigc::mem_fun(*this, (fn)));
 
 void Amba::initialize() {
 	auto& s2e = *this->s2e();
@@ -26,6 +26,11 @@ void Amba::initialize() {
 			*this,
 			&Amba::slotTranslateBlockStart
 		));
+	core.onTranslateInstructionStart
+		.connect(sigc::mem_fun(
+			*this,
+			&Amba::translateInstructionStart
+		));
 }
 
 void Amba::slotTranslateBlockStart(
@@ -38,13 +43,40 @@ void Amba::slotTranslateBlockStart(
 		this->getDebugStream(state) << "Translating block at " << hexval(pc) << "\n";
 	}
 	if (this->m_traceBlockExecution) {
-		signal->connect(sigc::mem_fun(*this, &Amba::slotExecuteBlockStart));
+		SUBSCRIBE(&Amba::slotExecuteBlockStart);
 	}
 }
 
 void Amba::slotExecuteBlockStart(S2EExecutionState *state, u64 pc) {
 	getDebugStream(state) << "Executing block at " << hexval(pc) << "\n";
 }
+
+void Amba::translateInstructionStart(
+	ExecutionSignal *signal,
+	S2EExecutionState *state,
+	TranslationBlock *tb,
+	u64 pc
+) {
+	const u64 op = memory[pc];
+	if (isCallOp(op)) {
+		SUBSCRIBE(&Amba::onFunctionCall);
+	}
+	if (isDerefOp(op)) {
+		SUBSCRIBE(&Amba::onDeref);
+	}
+}
+
+void Amba::onFunctionCall(S2EExecutionState *state, u64 pc) {
+	// if malloc
+		// Add return value + alloc size to this->m_allocations
+}
+
+void Amba::onDeref(S2EExecutionState *state, u64 pc) {
+	// Check if read adr is on stack or within saved heap data
+}
+
+static bool isCallOp(const u64 op_code) {}
+static bool isDerefOp(const u64 op_code) {}
 
 } // namespace plugins
 } // namespace s2e
