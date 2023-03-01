25aefcb5235faff179f21610ed7a1e36132dc922
Loke Gustafsson <lokegustafsson@pm.me>
2023-03-02 00:16:52 +0100

    crates/amba: almost successfully run qemu+s2e

--- crates/amba/src/run/session.rs
+++ crates/amba/src/run/session.rs
@@ -1,4 +1,8 @@
-use std::path::{Path, PathBuf};
+use std::{
+	error::Error,
+	fs,
+	path::{Path, PathBuf},
+};
 
 use serde::Serialize;
 use tera::{Context, Tera};
@@ -7,81 +11,135 @@ use crate::cmd::Cmd;
 
 #[derive(Serialize)]
 pub struct S2EConfig {
-	// Common
-	enable_pov_generation: bool,
-	use_seeds: bool,
-	enable_tickler: bool,
-	// For `s2e-config.lua`
+	creation_time: &'static str,
 	project_dir: PathBuf,
-	seeds_dir: Option<PathBuf>,
-	allow_s2eput_to_host: bool,
-	has_guestfs: bool,
-	guestfs_paths: Vec<PathBuf>,
+	use_seeds: bool,
+	project_name: &'static str,
 	modules: Vec<[String; 1]>,
 	processes: Vec<String>,
 	use_cupa: bool,
-	use_test_case_generator: bool,
-	recipes_dir: Option<PathBuf>,
-	enable_cfi: bool,
-	custom_lua_string: String,
-	// For `bootstrap.sh`
+	target_lua_template: &'static str,
+	custom_lua_string: &'static str,
 	project_type: &'static str,
 	image_arch: &'static str,
+	target_bootstrap_template: &'static str,
 	target: Target,
+	dynamically_linked: bool,
+	sym_args: Vec<u8>,
+	enable_pov_generation: bool,
+	enable_tickler: bool,
+	has_guestfs: bool,
+	guestfs_paths: Vec<PathBuf>,
+	use_test_case_generator: bool,
+	enable_cfi: bool,
 }
 #[derive(Serialize)]
 pub struct Target {
+	arch: &'static str,
 	name: String,
-	file_names_to_s2eget: Vec<String>,
-	args_space_concatenated_symbolic_file_names: String,
-	args_space_concatenated_all: String,
+	names: Vec<String>,
+	args: Args,
+}
+#[derive(Serialize)]
+pub struct Args {
+	symbolic_file_names: Vec<String>,
+	resolved_args: Vec<String>,
 }
 
-const TEMPLATE: &str = include_str!("../../template/template.s2e-config.lua");
-const LIBRARY: &str = include_str!("../../template/library.lua");
-const BOOTSTRAP: &str = include_str!("../../template/template.bootstrap.sh");
+const LIBRARY_LUA: &str = include_str!("../../data/library.lua");
+const TEMPLATE_DIR: &str = concat!(env!("AMBA_SRC_DIR"), "/crates/amba/templates");
 
 impl S2EConfig {
 	pub fn new(session_dir: &Path, executable_file_name: &str) -> Self {
 		Self {
-			enable_tickler: false,
+			creation_time: "CREATION_TIME",
 			project_dir: session_dir.to_owned(),
 			use_seeds: false,
-			seeds_dir: None,
-			allow_s2eput_to_host: false,
-			has_guestfs: false,
-			guestfs_paths: Vec::new(),
+			project_name: "PROJECT_NAME",
 			modules: vec![[executable_file_name.to_owned()]],
 			processes: vec![executable_file_name.to_owned()],
 			use_cupa: true,
-			enable_pov_generation: false,
-			use_test_case_generator: true,
-			recipes_dir: None,
-			enable_cfi: true,
-			custom_lua_string: "".to_string(),
+			target_lua_template: "s2e-config.linux.lua",
+			custom_lua_string: "",
 			project_type: "linux",
 			image_arch: "x86_64",
+			target_bootstrap_template: "bootstrap.linux.sh",
 			target: Target {
+				arch: "x86_64",
 				name: executable_file_name.to_owned(),
-				file_names_to_s2eget: vec![executable_file_name.to_owned()],
-				args_space_concatenated_symbolic_file_names: "".to_owned(),
-				args_space_concatenated_all: "".to_owned(),
+				names: vec![executable_file_name.to_owned()],
+				args: Args {
+					symbolic_file_names: vec![],
+					resolved_args: vec![],
+				},
 			},
+			dynamically_linked: true,
+			sym_args: vec![],
+			enable_pov_generation: false,
+			enable_tickler: false,
+			has_guestfs: false,
+			guestfs_paths: Vec::new(),
+			use_test_case_generator: true,
+			enable_cfi: false,
 		}
 	}
 
 	pub fn save_to(&self, cmd: &mut Cmd, session_dir: &Path) {
 		assert!(session_dir.exists());
-		let mut tera = Tera::default();
-		let context = Context::from_serialize(self).unwrap();
-		cmd.write(
-			session_dir.join("s2e-config.lua"),
-			tera.render_str(TEMPLATE, &context).unwrap(),
+		cmd.write(session_dir.join("library.lua"), LIBRARY_LUA);
+
+		assert!(
+			Path::new(TEMPLATE_DIR).exists(),
+			"non-existent {TEMPLATE_DIR}"
 		);
-		cmd.write(session_dir.join("library.lua"), LIBRARY);
-		cmd.write(
-			session_dir.join("bootstrap.sh"),
-			tera.render_str(BOOTSTRAP, &context).unwrap(),
-		);
+		let template_dir = fs::canonicalize(TEMPLATE_DIR).unwrap();
+		tracing::trace!(?template_dir, "Using templates from");
+		let mut renderer = Renderer {
+			cmd,
+			session_dir,
+			tera: match Tera::new(&format!("{}/*", template_dir.to_str().unwrap())) {
+				Ok(tera) => tera,
+				Err(err) => Renderer::handle_error("template dir", err),
+			},
+			context: &Context::from_serialize(self).unwrap(),
+		};
+		renderer.render("s2e-config.lua");
+		renderer.render("bootstrap.sh");
+	}
+}
+struct Renderer<'a> {
+	cmd: &'a mut Cmd,
+	session_dir: &'a Path,
+	tera: Tera,
+	context: &'a Context,
+}
+impl Renderer<'_> {
+	fn render(&mut self, name: &'static str) {
+		match self.tera.render(name, &self.context) {
+			Ok(content) => self.cmd.write(self.session_dir.join(name), content),
+			Err(err) => Self::handle_error(name, err),
+		}
+	}
+
+	fn handle_error(name: &'static str, err: tera::Error) -> ! {
+		let msg = match (&err.kind, err.source()) {
+			(tera::ErrorKind::Msg(msg), None) => Some(msg.to_owned()),
+			(tera::ErrorKind::Msg(msg), Some(inner)) => {
+				match (
+					inner.downcast_ref::<tera::Error>().map(|e| &e.kind),
+					inner.source(),
+				) {
+					(Some(tera::ErrorKind::Msg(inner_msg)), None) => {
+						Some(format!("{msg}\n{inner_msg}"))
+					}
+					_ => None,
+				}
+			}
+			_ => None,
+		};
+		if let Some(msg) = msg {
+			tracing::error!("Error within {name}:\n{msg}");
+		}
+		panic!("Tera::render failed at {err:#?}");
 	}
 }
