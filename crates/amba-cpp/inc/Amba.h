672aa3fec24290209f9dd508d047f18a4a4a0b5c
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-02 00:10:47 +0100

    Outline plan for implementation of overrun

--- crates/amba-cpp/inc/Amba.h
+++ crates/amba-cpp/inc/Amba.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "cpu/types.h"
 #include <s2e/CorePlugin.h>
 #include <s2e/Plugin.h>
 #include <s2e/S2EExecutionState.h>
+
+#include <unordered_map>
 
 namespace s2e {
 namespace plugins {
@@ -23,11 +26,16 @@ public:
 	void initialize();
 
 	TranslationFunction slotTranslateBlockStart;
+	TranslationFunction translateInstructionStart;
 	ExecutionFunction slotExecuteBlockStart;
+	ExecutionFunction onFunctionCall;
+	ExecutionFunction onDeref;
 
 private:
 	bool m_traceBlockTranslation;
 	bool m_traceBlockExecution;
+
+	std::unordered_map<target_phys_addr_t, target_phys_addr_t> m_allocations;
 };
 
 } // namespace plugins
