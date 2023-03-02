f2120450601c455c0eb397cf4cc4e1943549f1b6
Loke Gustafsson <lokegustafsson@pm.me>
2023-03-02 01:30:39 +0100

    crates/amba: kill child processes on ctrl-c

--- crates/amba/src/cmd.rs
+++ crates/amba/src/cmd.rs
@@ -1,10 +1,18 @@
 use std::{
+	collections::{hash_map, HashMap},
 	fs::{self, ReadDir},
-	io, iter,
+	io, iter, mem,
 	path::Path,
-	process::{Command, ExitStatus, Output, Stdio},
-	sync::atomic::{AtomicBool, Ordering},
+	process::{self, Child, Command, ExitStatus, Output, Stdio},
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		Mutex, TryLockError,
+	},
+	thread,
+	time::Duration,
 };
+
+static CHILDREN: Mutex<Option<HashMap<u32, Child>>> = Mutex::new(None);
 
 pub struct Cmd {
 	_no_construct: (),
@@ -15,16 +23,17 @@ impl Cmd {
 		ACQUIRED
 			.compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
 			.expect("Cmd::get() can only be called once");
+		ctrlc::set_handler(ctrlc_handler).unwrap();
 		Self { _no_construct: () }
 	}
 
 	pub fn command_spawn_wait(&mut self, command: &mut Command) -> ExitStatus {
 		tracing::trace!(
-		  cwd = ?command.get_current_dir(),
-		  env = ?command.get_envs().collect::<Vec<_>>(),
-		  args = ?iter::once(command.get_program()).chain(command.get_args()).collect::<Vec<_>>()
+			cwd = ?command.get_current_dir(),
+			env = ?command.get_envs().collect::<Vec<_>>(),
+			args = ?iter::once(command.get_program()).chain(command.get_args()).collect::<Vec<_>>()
 		);
-		command.spawn().unwrap().wait().unwrap()
+		safe_wait(command.spawn().unwrap()).wait().unwrap()
 	}
 
 	pub fn command_capture_stdout(&mut self, command: &mut Command) -> Result<Vec<u8>, Vec<u8>> {
@@ -32,13 +41,22 @@ impl Cmd {
 			cwd = ?command.get_current_dir(),
 			env = ?command.get_envs().collect::<Vec<_>>(),
 			args = ?iter::once(command.get_program()).chain(command.get_args()).collect::<Vec<_>>(),
-		"capturing stdout"
-		  );
+			"capturing stdout"
+		);
 		let Output {
 			status,
 			stdout,
 			stderr,
-		} = command.stderr(Stdio::inherit()).output().unwrap();
+		} = safe_wait(
+			command
+				.stdin(Stdio::piped())
+				.stdout(Stdio::piped())
+				.stderr(Stdio::inherit())
+				.spawn()
+				.unwrap(),
+		)
+		.wait_with_output()
+		.unwrap();
 		assert!(
 			stderr.is_empty(),
 			"stderr: '{}'",
@@ -96,4 +114,59 @@ impl Cmd {
 		tracing::trace!(?file, ?target, "copy_file");
 		fs::write(target, fs::read(file).unwrap()).unwrap()
 	}
+}
+
+fn safe_wait(child: Child) -> Child {
+	let id = child.id();
+	{
+		let mut slot = CHILDREN.try_lock().unwrap();
+		if slot.is_none() {
+			*slot = Some(HashMap::default());
+		}
+		slot.as_mut().unwrap().insert(id, child);
+	}
+	loop {
+		{
+			let mut guard = CHILDREN.try_lock().unwrap();
+			let mut entry = match guard.as_mut().unwrap().entry(id) {
+				hash_map::Entry::Occupied(occupied) => occupied,
+				hash_map::Entry::Vacant(_) => unreachable!(),
+			};
+			match entry.get_mut().try_wait().unwrap() {
+				Some(_) => return entry.remove(),
+				None => {}
+			}
+		}
+		thread::sleep(Duration::from_millis(50));
+	}
+}
+
+pub fn ctrlc_handler() {
+	println!();
+	let mut guard = match CHILDREN.try_lock() {
+		Ok(guard) => guard,
+		Err(TryLockError::WouldBlock) => {
+			tracing::error!("child process map is locked; we cannot kill our children and must leak them instead");
+			process::exit(2);
+		}
+		Err(TryLockError::Poisoned(poison)) => {
+			panic!("unexpected poison: {poison:#?}");
+		}
+	};
+	if let Some(map) = mem::take(&mut *guard) {
+		for (pid, mut child) in map {
+			match child.kill() {
+				Ok(()) => tracing::info!(pid, "handling SIGINT; killing child process"),
+				Err(err) if err.kind() == io::ErrorKind::InvalidInput => {
+					tracing::info!(
+						pid,
+						"handling SIGINT; child process already exited"
+					)
+				}
+				Err(err) => panic!("unexpected io error: {:#?}", err),
+			}
+		}
+	}
+	Box::leak(Box::new(guard));
+	process::exit(1);
 }
