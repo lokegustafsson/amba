5d18df0970db2d7575e5503fb568d5cca20330b8
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-02 23:49:03 +0100

    Add zydis,zycore to project

---
+++ nix/zycore.nix
@@ -0,0 +1,27 @@
+# https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/development/libraries/zydis/zycore.nix
+
+{ stdenv
+, fetchFromGitHub
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "zycore";
+  version = "1.4.1";
+
+  src = fetchFromGitHub {
+    owner = "zyantific";
+    repo = "zycore-c";
+    rev = "v${version}";
+    hash = "sha256-kplUgrYecymGxz92tEU6H+NNtcN/Ao/tmmqdVo2c7HA=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+
+  # The absolute paths set by the Nix CMake build manager confuse
+  # Zycore's config generation (which appends them to the package path).
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+    "-DCMAKE_INSTALL_INCLUDEDIR=include"
+  ];
+}
