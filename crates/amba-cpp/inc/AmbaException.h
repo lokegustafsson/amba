53b4ed2b85b1ed23db071761c78d25d92a965502
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-05 19:05:36 +0100

    amba-cpp/AmbaException: Fix throw and add assert

--- crates/amba-cpp/inc/AmbaException.h
+++ crates/amba-cpp/inc/AmbaException.h
@@ -4,4 +4,6 @@
 using AmbaException = std::tuple<int, char const*>;
 
 #define AMBA_THROW() \
-	(throw std::make_tuple(__LINE__, __FILE__))
+	{ throw std::make_tuple(__LINE__, __FILE__); }
+#define AMBA_ASSERT(_cond) \
+	{ if (!(_cond)) { AMBA_THROW(); } }
