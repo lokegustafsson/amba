1dd39e23fc202ceed996ffbe03d9bb94d2003fc7
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-06 19:39:51 +0100

    amba-cpp/Amba: Split Amba into Amba, AmbaPlugin, HeapLeak

--- crates/amba-cpp/inc/Amba.h
+++ crates/amba-cpp/inc/Amba.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include <cpu/types.h>
-#include <s2e/CorePlugin.h>
-#include <s2e/Plugin.h>
 #include <s2e/S2EExecutionState.h>
 
-#include <vector>
+#include <compare>
 #include <optional>
 
+#include "Numbers.h"
 #include "Zydis.h"
 
 namespace amba {
@@ -23,43 +22,13 @@ struct AddressLengthPair {
 constexpr size_t MAX_INSTRUCTION_LENGTH = 15; // bytes
 
 std::optional<target_phys_addr_t> readOperandAddress(const CPUX86State &cpu_state, const ZydisDecodedOperand operand);
+
 std::array<uint8_t, MAX_INSTRUCTION_LENGTH> readConstantMemory(S2EExecutionState *state, uint64_t pc);
+
 zydis::Instruction readInstruction(s2e::S2EExecutionState *state, u64 pc);
+
 bool isStackAddress(const CPUX86State &state, target_phys_addr_t adr);
+
 u64 readRegister(const CPUX86State &state, const ZydisRegister reg);
 
 } // namespace amba
-
-namespace s2e {
-namespace plugins {
-
-class Amba : public Plugin {
-	S2E_PLUGIN
-  public:
-	Amba(S2E *s2e) : Plugin(s2e) {}
-
-	using TranslationFunction = void (
-		s2e::ExecutionSignal *,
-		s2e::S2EExecutionState *state,
-		TranslationBlock *tb,
-		uint64_t p
-	);
-	using ExecutionFunction = void (s2e::S2EExecutionState *state, uint64_t pc);
-
-	void initialize();
-
-	TranslationFunction slotTranslateBlockStart;
-	TranslationFunction translateInstructionStart;
-	ExecutionFunction slotExecuteBlockStart;
-	ExecutionFunction onFunctionCall;
-	ExecutionFunction onDeref;
-
-  protected:
-	bool m_traceBlockTranslation;
-	bool m_traceBlockExecution;
-
-	std::vector<amba::AddressLengthPair> m_allocations;
-};
-
-} // namespace plugins
-} // namespace s2e
