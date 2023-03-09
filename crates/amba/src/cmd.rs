cb23484a87c9d627194471f3e62d4c5973c208af
Loke Gustafsson <lokegustafsson@pm.me>
2023-03-09 12:25:22 +0100

    crates/amba: upgrade many logs trace -> debug

--- crates/amba/src/cmd.rs
+++ crates/amba/src/cmd.rs
@@ -31,7 +31,7 @@ impl Cmd {
 	}
 
 	pub fn command_spawn_wait(&mut self, command: &mut Command) -> ExitStatus {
-		tracing::trace!(
+		tracing::debug!(
 			cwd = ?command.get_current_dir(),
 			env = ?command.get_envs().collect::<Vec<_>>(),
 			args = ?iter::once(command.get_program()).chain(command.get_args()).collect::<Vec<_>>()
@@ -40,7 +40,7 @@ impl Cmd {
 	}
 
 	pub fn command_capture_stdout(&mut self, command: &mut Command) -> Result<Vec<u8>, Vec<u8>> {
-		tracing::trace!(
+		tracing::debug!(
 			cwd = ?command.get_current_dir(),
 			env = ?command.get_envs().collect::<Vec<_>>(),
 			args = ?iter::once(command.get_program()).chain(command.get_args()).collect::<Vec<_>>(),
@@ -73,13 +73,13 @@ impl Cmd {
 
 	pub fn read_dir(&mut self, dir: impl AsRef<Path>) -> ReadDir {
 		let dir = dir.as_ref();
-		tracing::trace!(?dir, "read_dir");
+		tracing::debug!(?dir, "read_dir");
 		fs::read_dir(dir).unwrap()
 	}
 
 	pub fn remove_dir(&mut self, dir: impl AsRef<Path>) {
 		let dir = dir.as_ref();
-		tracing::trace!(?dir, "remove_dir");
+		tracing::debug!(?dir, "remove_dir");
 		fs::remove_dir(dir).unwrap()
 	}
 
@@ -89,38 +89,38 @@ impl Cmd {
 
 	pub fn try_remove_dir_all(&mut self, dir: impl AsRef<Path>) -> io::Result<()> {
 		let dir = dir.as_ref();
-		tracing::trace!(?dir, "remove_dir_all");
+		tracing::debug!(?dir, "remove_dir_all");
 		fs::remove_dir_all(dir)
 	}
 
 	pub fn create_dir_all(&mut self, dir: impl AsRef<Path>) {
 		let dir = dir.as_ref();
-		tracing::trace!(?dir, "create_dir_all");
+		tracing::debug!(?dir, "create_dir_all");
 		fs::create_dir_all(dir).unwrap()
 	}
 
 	pub fn write(&mut self, file: impl AsRef<Path>, content: impl AsRef<[u8]>) {
 		let file = file.as_ref();
-		tracing::trace!(?file, "write_file");
+		tracing::debug!(?file, "write_file");
 		fs::write(file, content).unwrap()
 	}
 
 	pub fn read(&mut self, file: impl AsRef<Path>) -> Vec<u8> {
 		let file = file.as_ref();
-		tracing::trace!(?file, "read_file");
+		tracing::debug!(?file, "read_file");
 		fs::read(file).unwrap()
 	}
 
 	pub fn remove(&mut self, file: impl AsRef<Path>) {
 		let file = file.as_ref();
-		tracing::trace!(?file, "remove_file");
+		tracing::debug!(?file, "remove_file");
 		fs::remove_file(file).unwrap()
 	}
 
 	pub fn copy(&mut self, file: impl AsRef<Path>, target: impl AsRef<Path>) {
 		let file = file.as_ref();
 		let target = target.as_ref();
-		tracing::trace!(?file, ?target, "copy_file");
+		tracing::debug!(?file, ?target, "copy_file");
 		fs::write(target, fs::read(file).unwrap()).unwrap()
 	}
 
@@ -131,7 +131,7 @@ impl Cmd {
 		mut url: Url,
 	) -> reqwest::blocking::Response {
 		for _ in 0..10 {
-			tracing::trace!(
+			tracing::debug!(
 				method = method.as_str(),
 				url = url.as_str(),
 				"HTTP"
