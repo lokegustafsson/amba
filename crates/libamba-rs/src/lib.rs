2d5b19dcfe760dc8c5c5ea609e67b2b8ef94a978
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-12 02:46:05 +0100

    libamba-rs: Fix function name spelling

--- crates/libamba-rs/src/lib.rs
+++ crates/libamba-rs/src/lib.rs
@@ -5,14 +5,14 @@ pub mod graph;
 use graph::ControlFlowGraph;
 
 #[no_mangle]
-pub extern "C" fn rust_create_control_flow_grapgh() -> *mut ControlFlowGraph {
+pub extern "C" fn rust_create_control_flow_graph() -> *mut ControlFlowGraph {
 	// Allocate a ControlFlowGraph by leaking a Box
 	let b = Box::new(ControlFlowGraph::default());
 	Box::into_raw(b) as _
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn rust_free_control_flow_grapgh(ptr: *mut ControlFlowGraph) {
+pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut ControlFlowGraph) {
 	// Deallocate and run destructors by converting it into a Box and letting std handle it
 	let _ = Box::from_raw(ptr);
 }
