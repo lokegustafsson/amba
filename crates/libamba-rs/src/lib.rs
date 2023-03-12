541c66c3aef3b7ed56d19d2e4290d6cacdee923b
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-12 20:47:07 +0100

    libamba/Test: Call rust code

--- crates/libamba-rs/src/lib.rs
+++ crates/libamba-rs/src/lib.rs
@@ -19,5 +19,7 @@ pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut ControlFlowGraph
 
 #[no_mangle]
 pub extern "C" fn rust_main() -> std::ffi::c_int {
+	println!("Hello world");
+	let p = rust_create_control_flow_graph();
 	0
 }
