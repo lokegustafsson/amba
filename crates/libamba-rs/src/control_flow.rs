2be6261b217add78f3d410651906d92e376ead0c
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-13 00:24:28 +0100

    libamba/control_flow: Update method

--- crates/libamba-rs/src/control_flow.rs
+++ crates/libamba-rs/src/control_flow.rs
@@ -11,4 +11,48 @@ impl ControlFlowGraph {
 	pub fn new() -> Self {
 		Default::default()
 	}
+
+	/// Insert a node connection. Returns true if the connection
+	/// is new.
+	pub fn update(&mut self, from: u64, to: u64) -> bool {
+		let mut modified = false;
+		self.graph
+			.0
+			.entry(from)
+			.and_modify(|node| {
+				modified |= node.to.insert(to);
+			})
+			.or_insert_with(|| {
+				modified = true;
+				Block {
+					id: from,
+					to: [to].into_iter().collect(),
+					from: Default::default(),
+				}
+			});
+		self.graph
+			.0
+			.entry(to)
+			.and_modify(|node| {
+				modified |= node.from.insert(from);
+			})
+			.or_insert_with(|| {
+				modified = true;
+				Block {
+					id: to,
+					to: Default::default(),
+					from: [from].into_iter().collect(),
+				}
+			});
+
+		if modified {
+			// Do cleverly when performance actually needs it
+			self.compressed_graph = self.graph.clone();
+			self.compressed_graph.compress();
+		}
+
+		modified
+	}
+}
+
 }
