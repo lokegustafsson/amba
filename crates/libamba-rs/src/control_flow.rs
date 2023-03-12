f42ccc72a22860eca82053624ffede72e75edb13
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-13 00:24:47 +0100

    libamba-rs/control_flow: Test1

--- crates/libamba-rs/src/control_flow.rs
+++ crates/libamba-rs/src/control_flow.rs
@@ -55,4 +55,39 @@ impl ControlFlowGraph {
 	}
 }
 
+#[cfg(test)]
+mod test {
+	use crate::control_flow::*;
+
+	#[test]
+	fn test1() {
+		let mut cfg = ControlFlowGraph::new();
+		// 0 → 1
+		assert!(cfg.update(0, 1));
+		assert_eq!(cfg.graph.len(), 2);
+		assert_eq!(cfg.compressed_graph.len(), 1);
+		assert!(!cfg.update(0, 1));
+
+		// 0 → 1 → 2
+		assert!(cfg.update(1, 2));
+		assert_eq!(cfg.graph.len(), 3);
+		assert_eq!(cfg.compressed_graph.len(), 1);
+		assert!(!cfg.update(1, 2));
+
+		// 0 → 1 → 2
+		//     ↓
+		//     3
+		assert!(cfg.update(1, 3));
+		assert_eq!(cfg.graph.len(), 4);
+		assert_eq!(cfg.compressed_graph.len(), 3);
+		assert!(!cfg.update(1, 3));
+
+		// 0 → 1 → 2
+		// ↑   ↓
+		// 4   3
+		assert!(cfg.update(4, 0));
+		assert_eq!(cfg.graph.len(), 5);
+		assert_eq!(cfg.compressed_graph.len(), 3);
+		assert!(!cfg.update(4, 0));
+	}
 }
