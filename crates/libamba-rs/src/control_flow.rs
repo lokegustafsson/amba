f258a8aa79d05efd2cecb926891ce8effd6f9844
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-14 12:47:32 +0100

    libamba-rs/graph, control_flow: Move update body into graph

--- crates/libamba-rs/src/control_flow.rs
+++ crates/libamba-rs/src/control_flow.rs
@@ -15,35 +15,7 @@ impl ControlFlowGraph {
 	/// Insert a node connection. Returns true if the connection
 	/// is new.
 	pub fn update(&mut self, from: u64, to: u64) -> bool {
-		let mut modified = false;
-		self.graph
-			.0
-			.entry(from)
-			.and_modify(|node| {
-				modified |= node.to.insert(to);
-			})
-			.or_insert_with(|| {
-				modified = true;
-				Block {
-					id: from,
-					to: [to].into_iter().collect(),
-					from: Default::default(),
-				}
-			});
-		self.graph
-			.0
-			.entry(to)
-			.and_modify(|node| {
-				modified |= node.from.insert(from);
-			})
-			.or_insert_with(|| {
-				modified = true;
-				Block {
-					id: to,
-					to: Default::default(),
-					from: [from].into_iter().collect(),
-				}
-			});
+		let modified = self.graph.update(from, to);
 
 		if modified {
 			// Do cleverly when performance actually needs it
