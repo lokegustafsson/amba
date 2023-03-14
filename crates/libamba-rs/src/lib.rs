ffb0aeeebfc0f1285ed456efddedd6f1a0201d29
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-14 15:22:35 +0100

    libamba-rs/lib: Move ffi functions into ffi module

--- crates/libamba-rs/src/lib.rs
+++ crates/libamba-rs/src/lib.rs
@@ -1,26 +1,27 @@
-#![allow(unsafe_code)]
-
-pub mod graph;
 pub mod control_flow;
+pub mod graph;
 
-use control_flow::ControlFlowGraph;
+#[allow(unsafe_code, clippy::missing_safety_doc)]
+mod ffi {
+	use crate::control_flow::ControlFlowGraph;
 
-#[no_mangle]
-pub extern "C" fn rust_create_control_flow_graph() -> *mut ControlFlowGraph {
-	// Allocate a ControlFlowGraph by leaking a Box
-	let b = Box::new(ControlFlowGraph::new());
-	Box::into_raw(b)
-}
+	#[no_mangle]
+	pub extern "C" fn rust_create_control_flow_graph() -> *mut ControlFlowGraph {
+		// Allocate a ControlFlowGraph by leaking a Box
+		let b = Box::new(ControlFlowGraph::new());
+		Box::into_raw(b)
+	}
 
-#[no_mangle]
-pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut ControlFlowGraph) {
-	// Deallocate and run destructors by converting it into a Box and letting std handle it
-	let _ = Box::from_raw(ptr);
-}
+	#[no_mangle]
+	pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut ControlFlowGraph) {
+		// Deallocate and run destructors by converting it into a Box and letting std handle it
+		let _ = Box::from_raw(ptr);
+	}
 
-#[no_mangle]
-pub extern "C" fn rust_main() -> std::ffi::c_int {
-	println!("Hello world");
-	let p = rust_create_control_flow_graph();
-	0
+	#[no_mangle]
+	pub extern "C" fn rust_main() -> std::ffi::c_int {
+		println!("Hello world");
+		let p = rust_create_control_flow_graph();
+		0
+	}
 }
