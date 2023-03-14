ac140add67004ae8dc02a6fad31a794c665bad22
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-14 16:53:49 +0100

    libamba-rs/lib: Move ffi code into ffi module

--- crates/libamba-rs/src/lib.rs
+++ crates/libamba-rs/src/lib.rs
@@ -1,4 +1,8 @@
-#[no_mangle]
-pub extern "C" fn rust_main() -> std::ffi::c_int {
-	0
+#[allow(unsafe_code, clippy::missing_safety_doc)]
+mod ffi {
+	#[no_mangle]
+	pub extern "C" fn rust_main() -> std::ffi::c_int {
+		println!("Hello world");
+		0
+	}
 }
