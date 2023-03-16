c48ad6649194d3a5ac7c10212adee96160362ee4
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-17 00:21:00 +0100

    libamba-rs/graph: Stop renaming nodes and keep a graph-wide rename map instead
    asjdhask
    
    SQUASHME: Fix 0 -> nodes

--- crates/libamba-rs/src/control_flow.rs
+++ crates/libamba-rs/src/control_flow.rs
@@ -39,8 +39,8 @@ impl ControlFlowGraph {
 		// Only edit the compressed graph if this was a new link
 		if modified {
 			// If both links exist we can just add this one link
-			if self.compressed_graph.0.contains_key(&to)
-				&& self.compressed_graph.0.contains_key(&from)
+			if self.compressed_graph.nodes.contains_key(&to)
+				&& self.compressed_graph.nodes.contains_key(&from)
 			{
 				self.compressed_graph.update(from, to);
 			} else {
@@ -76,10 +76,10 @@ impl fmt::Display for ControlFlowGraph {
 			"\nNodes: {} ({})\nEdges: {} ({})\nConnections: Avg: {}, Max: {}\nUpdates: {}\nRebuilds: {}\nLifetime: {:?}\nTime spent rebuilding: {:?}",
 			g.len(),
 			self.graph.len(),
-			g.0.values().map(|b| b.from.len()).sum::<usize>(),
-			self.graph.0.values().map(|b| b.from.len()).sum::<usize>(),
-			g.0.values().map(|b| b.from.len()).sum::<usize>() as f64 / g.len() as f64,
-			g.0.values().map(|b| b.from.len()).max().unwrap(),
+			g.nodes.values().map(|b| b.from.len()).sum::<usize>(),
+			self.graph.nodes.values().map(|b| b.from.len()).sum::<usize>(),
+			g.nodes.values().map(|b| b.from.len()).sum::<usize>() as f64 / g.len() as f64,
+			g.nodes.values().map(|b| b.from.len()).max().unwrap(),
 			self.updates,
 			self.rebuilds,
 			now - self.spawned_at,
