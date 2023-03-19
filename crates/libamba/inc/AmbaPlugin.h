3bce36e0446ca02080e9647866b48313bd5adbf3
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-19 18:10:17 +0100

    libamba/AmbaPlugin: Rename m_control_flow → m_assembly_graph

--- crates/libamba/inc/AmbaPlugin.h
+++ crates/libamba/inc/AmbaPlugin.h
@@ -30,7 +30,7 @@ class AmbaPlugin : public Plugin {
 
   protected:
 	heap_leak::HeapLeak m_heap_leak;
-	control_flow::ControlFlow m_control_flow;
+	control_flow::ControlFlow m_assembly_graph;
 };
 
 } // namespace plugins
