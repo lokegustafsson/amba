abacdde3a653f1a742e8d7bf43daa21b64706eff
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-19 19:12:48 +0100

    libamba-rs/lib: Hide C interfaces behind mutex

--- crates/libamba-rs/src/lib.rs
+++ crates/libamba-rs/src/lib.rs
@@ -5,21 +5,23 @@ pub mod small_set;
 
 #[allow(unsafe_code, clippy::missing_safety_doc)]
 mod ffi {
+	use std::sync::Mutex;
+
 	use crate::control_flow::ControlFlowGraph;
 
 	/// Create a newly allocated `ControlFlowGraph` and return an
 	/// owning raw pointer. This pointer may only be freed with
 	/// the `rust_free_control_flow_graph` function.
 	#[no_mangle]
-	pub extern "C" fn rust_new_control_flow_graph() -> *mut ControlFlowGraph {
-		Box::into_raw(Box::new(ControlFlowGraph::new()))
+	pub extern "C" fn rust_new_control_flow_graph() -> *mut Mutex<ControlFlowGraph> {
+		Box::into_raw(Box::new(Mutex::new(ControlFlowGraph::new())))
 	}
 
 	/// Free a `ControlFlowGraph` allocated by
 	/// `rust_new_control_flow_graph`. After this fuction has been
 	/// called the pointer may not be used again.
 	#[no_mangle]
-	pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut ControlFlowGraph) {
+	pub unsafe extern "C" fn rust_free_control_flow_graph(ptr: *mut Mutex<ControlFlowGraph>) {
 		let _ = Box::from_raw(ptr);
 	}
 
@@ -29,17 +31,19 @@ mod ffi {
 	/// has changed.
 	#[no_mangle]
 	pub unsafe extern "C" fn rust_update_control_flow_graph(
-		ptr: *mut ControlFlowGraph,
+		ptr: *mut Mutex<ControlFlowGraph>,
 		from: u64,
 		to: u64,
 	) -> bool {
-		let cfg = &mut *ptr;
+		let mutex = &*ptr;
+		let mut cfg = mutex.lock().unwrap();
 		cfg.update(from, to)
 	}
 
 	#[no_mangle]
-	pub unsafe extern "C" fn rust_print_graph_size(ptr: *mut ControlFlowGraph) {
-		let cfg = &*ptr;
+	pub unsafe extern "C" fn rust_print_graph_size(ptr: *mut Mutex<ControlFlowGraph>) {
+		let mutex = &*ptr;
+		let cfg = mutex.lock().unwrap();
 		println!("{cfg}");
 	}
 
