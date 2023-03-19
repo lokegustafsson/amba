7389039f186342397bf0227818a2664a712f3a78
Samuel Kyletoft <samuel@kyletoft.se>
2023-03-19 02:31:55 +0100

    Makefile: Move gen_compile_flags.sh into makerule

--- gen_compile_flags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Generates the compile_flags.txt for libamba by evoking the make file target.
-
-set -e
-
-script_parent=$(dirname  -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
-cd "$script_parent"
-make -j -sC crates/libamba/ compile_flags.txt
