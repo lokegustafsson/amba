c326c52cb4ad26aee5281a433cd814414a2cdc11
Enayatullah Norozi <enayathushmand12@gmail.com>
2023-03-28 17:54:32 +0200

    disassembler: line::Context move out cache and refactor.

--- crates/disassembler/src/line.rs
+++ crates/disassembler/src/line.rs
@@ -1,7 +1,6 @@
 use std::{
 	collections::HashMap,
-	fmt, fs,
-	io::BufRead,
+	fmt, fs, io,
 	path::{Path, PathBuf},
 	rc::Rc,
 };
@@ -17,17 +16,61 @@ type Addr2LineContext = addr2line::Context<EndianReader<RunTimeEndian, Rc<[u8]>>
 #[derive(Error, Debug)]
 pub enum Error {
 	#[error("{0}")]
-	Io(#[from] std::io::Error),
+	Io(#[from] io::Error),
 	#[error("{0}")]
 	Gimli(#[from] addr2line::gimli::Error),
 	#[error("{0}")]
 	ObjectRead(#[from] read::Error),
+	#[error("{0}")]
+	Custom(&'static str),
+}
+
+#[derive(Default)]
+pub struct FileLineCache {
+	/// (line_starts, Cached source files that have already been opened).
+	files: HashMap<PathBuf, (Vec<usize>, String)>,
+}
+
+impl FileLineCache {
+	pub fn get<'a>(
+		&'a mut self,
+		filepath: &Path,
+		linenumber: u32,
+	) -> Result<Option<&'a str>, io::Error> {
+		self.populate(filepath)?;
+		let (line_start_indices, content) = &self.files[filepath];
+		let ret = (|| {
+			let this_line = *line_start_indices.get(linenumber as usize - 1)?;
+			let next_line = line_start_indices
+				.get(linenumber as usize)
+				.map_or(content.len(), |&idx| idx);
+			Some(&content[this_line..next_line])
+		})();
+
+		Ok(ret)
+	}
+
+	fn populate(&mut self, filepath: &Path) -> Result<(), io::Error> {
+		if self.files.contains_key(filepath) {
+			return Ok(());
+		}
+		let content = fs::read_to_string(filepath)?;
+		let mut line_start_indices = Vec::new();
+		let mut current_index = 0;
+		for line in content.lines() {
+			line_start_indices.push(current_index);
+			current_index += line.len();
+		}
+		self.files
+			.insert(filepath.to_owned(), (line_start_indices, content));
+		Ok(())
+	}
 }
 
 /// For caching source files loaded into memory. So that source code lines can be fetched without
 /// rereading the source code files.
 pub struct Context {
-	files: HashMap<PathBuf, Vec<u8>>, // Cached source files that have already been opened.
+	cache: FileLineCache, // Cached source files that have already been opened.
 	addr2line_context: Addr2LineContext,
 }
 
@@ -35,7 +78,7 @@ impl Context {
 	// filepath to the binary.
 	pub fn new(filepath: &Path) -> Result<Self, Error> {
 		Ok(Self {
-			files: HashMap::new(),
+			cache: FileLineCache::default(),
 			addr2line_context: Context::create_addr2line_context(filepath)?,
 		})
 	}
@@ -43,29 +86,57 @@ impl Context {
 	/// Returns the line of source code corresponding to `addr`, if location information for `addr`
 	/// exist, Ok(Some(String)) is returned, if location information doesn't exist in debug
 	/// information, Ok(None) is returned, otherwise any errors are propagated as our `Error` enum.
-	pub fn get_source_line(&mut self, addr: u64) -> Result<Option<String>, Error> {
+	pub fn get_source_line(&mut self, addr: u64) -> Result<Option<&str>, Error> {
 		let line_info = self.addr2loc(addr)?;
 		let Some((file_name, line, _)) = line_info else { return Ok(None); };
 
 		let filepath = Path::new(&file_name);
-		let file = if let Some(file) = self.files.get(filepath) {
-			file
-		} else {
-			let path = filepath.to_path_buf();
-			let data = fs::read(filepath)?;
+		let res = self.cache.get(filepath, line)?;
+		Ok(res)
+	}
 
-			self.files.entry(path).or_insert(data)
-		};
-		let res = file.lines().nth(line as usize - 1).transpose()?;
+	pub fn get_source_lines(
+		&mut self,
+		probe_low: u64,
+		probe_high: u64,
+	) -> Result<Vec<(u64, u64, addr2line::Location<'_>, String)>, Error> {
+		let mut loc_range_iter = self
+			.addr2line_context
+			.find_location_range(probe_low, probe_high)?
+			.peekable();
 
+		if let Some((_, _, loc)) = loc_range_iter.peek() {
+			if loc.file.is_none() {
+				return Err(Error::Custom(
+					"Debug data has no source file reference. Stripped?",
+				));
+			}
+			if loc.line.is_none() {
+				return Err(Error::Custom(
+					"Debug data has no source code line reference.",
+				));
+			}
+			self.cache
+				.get(loc.file.unwrap().as_ref(), loc.line.unwrap())?;
+		}
+
+		let mut res = Vec::new();
+		for (start_addr, size, loc) in loc_range_iter {
+			let item = self
+				.cache
+				.get(loc.file.unwrap().as_ref(), loc.line.unwrap())?;
+			if let Some(line) = item {
+				res.push((start_addr, size, loc, line.to_owned()));
+			}
+		}
 		Ok(res)
 	}
 
 	/// Get the `Ok(Some((filepath, line, column))` corresponding to an address. If no location
 	/// information for the `addr` is found, `Ok(None)` is returned. Other errors from addr2line is
 	/// otherwise propagated and wrapped in our `Error`.
-	pub fn addr2loc(&self, addr: u64) -> Result<Option<(String, u32, u32)>, Error> {
-		let mut locs = self.addr2line_context.find_frames(addr)?;
+	pub fn addr2loc(&self, probe: u64) -> Result<Option<(String, u32, u32)>, Error> {
+		let mut locs = self.addr2line_context.find_frames(probe)?;
 		let frame = locs.next()?.and_then(|frame| {
 			let location = frame.location?;
 			Some((
@@ -128,5 +199,21 @@ mod test {
 			line.unwrap(),
 			read_line("../../demos/hello.c", 4).unwrap()
 		);
+	}
+
+	#[test]
+	#[ignore]
+	fn locs() {
+		let binary_filepath = Path::new("../../demos/hello");
+		let low = 0x401180;
+		let high = 0x401190;
+		let mut context = Context::new(binary_filepath).unwrap();
+		let res = context.get_source_lines(low, high).unwrap();
+		let mut res2 = Vec::new();
+		res2 = res
+			.iter()
+			.map(|(start, size, _, line)| (start, size, line.clone()))
+			.collect();
+		dbg!(res2);
 	}
 }
