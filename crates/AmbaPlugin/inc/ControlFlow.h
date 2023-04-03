179d66221be4f445e15a464d22081a09e57c5c3d
Samuel Kyletoft <samuel@kyletoft.se>
2023-04-03 22:51:20 +0200

    libamba/ControlFlow: Add members to handle uuid mapping

--- crates/AmbaPlugin/inc/ControlFlow.h
+++ crates/AmbaPlugin/inc/ControlFlow.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "Numbers.h"
 #include "Amba.h"
 #include "LibambaRs.h"
@@ -19,6 +21,8 @@ class ControlFlow {
   protected:
 	u64 m_last;
   std::string m_name;
+	u64 m_last_uuid = 0;
+	std::unordered_map<u32, u32> m_uuids {};
 	ControlFlowGraph *m_cfg;
 };
 
