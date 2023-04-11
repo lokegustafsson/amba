137b8117b7f820b6322637f4d7abb1c3077b7e4c
lummoxx <clarasalberg@live.com>
2023-04-11 17:57:15 +0200

    sort vectors in strongly connected components

--- crates/data-structures/src/graph.rs
+++ crates/data-structures/src/graph.rs
@@ -261,23 +261,30 @@ impl Graph {
 
 	/// Returns the id of the merged node
 	pub fn merge_nodes(&mut self, l: u64, r: u64) -> u64 {
-		/*if l > r {
-			return self.merge_nodes(r, l);
-		}*/
+
 		if l == r {
 			return l;
 		}
-
-		assert!(self.nodes.contains_key(&l));
+        let leftmerge = l < r;
+        
+        assert!(self.nodes.contains_key(&l));
 		assert!(self.nodes.contains_key(&r));
 
-		let are_loop = self.are_loop(l, r);
-		let map = &mut self.nodes;
+        let are_loop; 
+		if leftmerge {
+            are_loop = self.are_loop(l, r);
+        }else{
+            are_loop = self.are_loop(r, l);
+        }
+        
+        // Must be after are_loop
+		//self.merges.insert(r, l);
+		//self.merges.remove(&l);
 
-		// Must be after are_loop
-		self.merges.insert(r, l);
-		self.merges.remove(&l);
 
+        let map = &mut self.nodes;
+
+		
 		let combine_sets = |mut left_set: SmallU64Set, mut right_set: SmallU64Set| -> SmallU64Set {
 			if left_set.len() < right_set.len() {
 				mem::swap(&mut left_set, &mut right_set);
@@ -291,36 +298,78 @@ impl Graph {
 
 		// Take the union of both nodes' input and then remove
 		// the nodes themselves
-		let r_ = map.get_mut(&r).unwrap();
-		let to_r = mem::take(&mut r_.to);
+        let r_;
+        if leftmerge {
+            // Must be after are_loop
+		    self.merges.insert(r, l);
+		    self.merges.remove(&l);
+		    
+            r_ = map.get_mut(&r).unwrap();
+        }else{
+            // Must be after are_loop
+		    self.merges.insert(l, r);
+		    self.merges.remove(&r);
+		
+            r_ = map.get_mut(&l).unwrap();
+        }
+		
+        let to_r = mem::take(&mut r_.to);
 		let from_r = mem::take(&mut r_.from);
 		let mut of_r = mem::take(&mut r_.of);
-
-		let l_ = map.get_mut(&l).unwrap();
+        let l_;
+        
+        if leftmerge{
+		    l_ = map.get_mut(&l).unwrap();
+        }else{
+		    l_ = map.get_mut(&r).unwrap();
+        }
 		let to_l = mem::take(&mut l_.to);
 		let from_l = mem::take(&mut l_.from);
 		let mut of_l = mem::take(&mut l_.of);
-
+        
 		l_.to = combine_sets(to_l, to_r);
 		l_.from = combine_sets(from_l, from_r);
-		l_.of.append(&mut of_l);// = combine_sets(of_l, of_r);
-        l_.of.append(&mut of_r);
+		
+        if leftmerge{
+            l_.of.append(&mut of_l);// = combine_sets(of_l, of_r);
+            l_.of.append(&mut of_r);
+        }else{
+            l_.of.append(&mut of_r);// = combine_sets(of_l, of_r);
+            l_.of.append(&mut of_l);
+        }
 
-		for parent in l_.of.iter() {
-			l_.to.remove(parent);
-			l_.from.remove(parent);
+        for parent in l_.of.iter() {
+		    l_.to.remove(parent);
+		    l_.from.remove(parent);
 		}
 
-		// Restore loop if they were a loop beforehand
-		if are_loop {
-			l_.from.insert(l);
-			l_.to.insert(l);
-		}
+		if leftmerge {
 
-		// Remove the right node from the graph
-		map.remove(&r);
+		    // Restore loop if they were a loop beforehand
+		    if are_loop {
+		        l_.from.insert(l);
+			    l_.to.insert(l);
+	    	}
 
-		l
+		    // Remove the right node from the graph
+		    map.remove(&r);
+                
+            l
+
+        }else{
+		    
+            // Restore loop if they were a loop beforehand
+		    if are_loop {
+		         l_.from.insert(r);
+			     l_.to.insert(r);
+	    	}
+
+		    // Remove the right node from the graph
+		    map.remove(&l);
+
+            r
+        }
+
 	}
 
 	fn edges(&self) -> impl Iterator<Item = (u64, u64)> + '_ {
@@ -406,6 +455,7 @@ impl Graph {
 						break;
 					}
 				}
+                new_node.of.sort_unstable();
 				state.out.insert(v, new_node);
 			}
 		}
@@ -415,7 +465,9 @@ impl Graph {
 				strong_connect(self, &mut state, node.id);
 			}
 		}
-
+        /*for node in state.out.keys(){
+            state.out.get_mut(&node).unwrap().of.sort_unstable();
+        }*/
 		state.out
 	}
 
@@ -503,7 +555,17 @@ impl Graph {
 		for u in l.into_iter().rev() {
 			assign(self, &mut acc, &mut assigned, u, u);
 		}
-
+        let keys = acc.keys().cloned().collect::<Vec<_>>();//clone();
+        for key in keys{
+            let res = acc.get_mut(&key);
+            match res {
+                Some(node) => {
+                    node.of.sort_unstable();
+                }
+                None => {
+                }
+            }
+        }
 		acc
 	}
 
@@ -716,7 +778,7 @@ mod test {
 			.collect(),
 		);
 		let expected =
-			Graph::with_nodes([(2, (2, [], [], [2, 1, 0]).into())].into_iter().collect());
+			Graph::with_nodes([(0, (0, [], [], [2, 1, 0]).into())].into_iter().collect());
 		graph.verify();
 		expected.verify();
 		graph.compress();
@@ -754,7 +816,7 @@ mod test {
 				.into_iter()
 				.collect(),
 		);
-		let expected = Graph::with_nodes([(1, (1, [], [], [1, 0]).into())].into_iter().collect());
+		let expected = Graph::with_nodes([(0, (0, [], [], [1, 0]).into())].into_iter().collect());
 		graph.verify();
 		expected.verify();
 		graph.compress();
@@ -837,10 +899,10 @@ mod test {
 		);
 		let expected = Graph::with_nodes(
 			[
-				(1, (1, [6], [3], [1]).into()),
-				(2, (2, [6], [3], [2]).into()),
+				(0, (0, [], [1, 2], [6, 5, 4, 0]).into()),
+                (1, (1, [0], [3], [1]).into()),
+				(2, (2, [0], [3], [2]).into()),
 				(3, (3, [1, 2], [], [3]).into()),
-				(6, (6, [], [1, 2], [6, 5, 4, 0]).into()),
 
 			]
 			.into_iter()
@@ -877,9 +939,9 @@ mod test {
 		let expected = Graph::with_nodes(
 			[
 				(0, (0, [1, 2], [], [0]).into()),
-				(1, (1, [4], [0], [1]).into()),
-				(2, (2, [4], [0], [2]).into()),
-				(4, (4, [], [1, 2], [4, 5, 6, 3]).into()),
+				(1, (1, [3], [0], [1]).into()),
+				(2, (2, [3], [0], [2]).into()),
+				(3, (3, [], [1, 2], [4, 5, 6, 3]).into()),
 			]
 			.into_iter()
 			.collect(),
@@ -955,11 +1017,11 @@ mod test {
 		);
 		let expected = Graph::with_nodes(
 			[
-				(0, (0, [3], [], [0]).into()),
-				(1, (1, [3], [], [1]).into()),
-				(3, (3, [4, 5], [0, 1], [3, 2]).into()),
-				(4, (4, [], [3], [4]).into()),
-				(5, (5, [], [3], [5]).into()),
+				(0, (0, [2], [], [0]).into()),
+				(1, (1, [2], [], [1]).into()),
+				(2, (2, [4, 5], [0, 1], [3, 2]).into()),
+				(4, (4, [], [2], [4]).into()),
+				(5, (5, [], [2], [5]).into()),
 			]
 			.into_iter()
 			.collect(),
@@ -1020,7 +1082,7 @@ mod test {
 			.collect(),
 		);
 		let expected = Graph::with_nodes(
-			[(2, (2, [2], [2], [2, 1, 0, 3]).into())]
+			[(0, (0, [0], [0], [2, 1, 0, 3]).into())]
 				.into_iter()
 				.collect(),
 		);
@@ -1102,7 +1164,7 @@ mod test {
 			.collect(),
 		);
 		let expected =
-			Graph::with_nodes([(2, (2, [], [], [2, 1, 0]).into())].into_iter().collect());
+			Graph::with_nodes([(0, (0, [], [], [2, 1, 0]).into())].into_iter().collect());
 		graph.verify();
 		expected.verify();
 		graph.compress_with_hint([0, 1].into_iter().collect());
@@ -1185,10 +1247,10 @@ mod test {
 		);
 		let expected = Graph::with_nodes(
 			[
-				(1, (1, [6], [3], [1]).into()),
-				(2, (2, [6], [3], [2]).into()),
+				(0, (0, [], [1, 2], [6, 5, 4, 0]).into()),
+                (1, (1, [0], [3], [1]).into()),
+				(2, (2, [0], [3], [2]).into()),
 				(3, (3, [1, 2], [], [3]).into()),
-				(6, (6, [], [1, 2], [6, 5, 4, 0]).into()),
 
 			]
 			.into_iter()
@@ -1225,9 +1287,9 @@ mod test {
 		let expected = Graph::with_nodes(
 			[
 				(0, (0, [1, 2], [], [0]).into()),
-				(1, (1, [4], [0], [1]).into()),
-				(2, (2, [4], [0], [2]).into()),
-				(4, (4, [], [1, 2], [4, 5, 6, 3]).into()),
+				(1, (1, [3], [0], [1]).into()),
+				(2, (2, [3], [0], [2]).into()),
+				(3, (3, [], [1, 2], [4, 5, 6, 3]).into()),
 			]
 			.into_iter()
 			.collect(),
@@ -1303,11 +1365,11 @@ mod test {
 		);
 		let expected = Graph::with_nodes(
 			[
-				(0, (0, [3], [], [0]).into()),
-				(1, (1, [3], [], [1]).into()),
-				(3, (3, [4, 5], [0, 1], [3, 2]).into()),
-				(4, (4, [], [3], [4]).into()),
-				(5, (5, [], [3], [5]).into()),
+				(0, (0, [2], [], [0]).into()),
+				(1, (1, [2], [], [1]).into()),
+				(2, (2, [4, 5], [0, 1], [3, 2]).into()),
+				(4, (4, [], [2], [4]).into()),
+				(5, (5, [], [2], [5]).into()),
 			]
 			.into_iter()
 			.collect(),
@@ -1368,7 +1430,7 @@ mod test {
 			.collect(),
 		);
 		let expected = Graph::with_nodes(
-			[(1, (1, [1], [1], [1, 0, 3, 2]).into())]
+			[(0, (0, [0], [0], [1, 0, 3, 2]).into())]
 				.into_iter()
 				.collect(),
 		);
