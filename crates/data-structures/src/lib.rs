91ab2c55d53add8fe9c25fdad025c71927ed02be
Samuel Kyletoft <samuel@kyletoft.se>
2023-04-16 02:52:47 +0200

    data-structures: rustfmt

--- crates/data-structures/src/lib.rs
+++ crates/data-structures/src/lib.rs
@@ -8,5 +8,5 @@ pub use control_flow::ControlFlowGraph;
 pub use disjoint_sets::DisjointSets;
 pub use embed::{Graph2D, Node2D};
 pub use graph::{Graph, Node};
-pub use small_set::SmallU64Set;
 pub use ipc::GraphIpc;
+pub use small_set::SmallU64Set;
