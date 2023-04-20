41ba41703b38ab3bf8d0f0346a7c4025381650b8
Samuel Kyletoft <samuel@kyletoft.se>
2023-04-21 01:21:53 +0200

    amba/run: Remove imports and make modules public

--- crates/amba/src/run/mod.rs
+++ crates/amba/src/run/mod.rs
@@ -1,30 +1,4 @@
-//! The run subcommand
-
-#![allow(unsafe_code)]
-
-use std::{
-	ffi::{OsStr, OsString},
-	mem,
-	net::Shutdown,
-	os::unix::{
-		net::{UnixListener, UnixStream},
-		process::CommandExt,
-	},
-	path::Path,
-	process::{self, Command, ExitStatus},
-	sync::{mpsc, Arc, Mutex, RwLock},
-	thread::{self, ScopedJoinHandle},
-	time::{Duration, Instant},
-};
-
-use eframe::egui::Context;
-use graphui::{EmbeddingParameters, Graph2D};
-use ipc::{GraphIpc, IpcError, IpcMessage};
-use qmp_client::{QmpClient, QmpCommand, QmpError, QmpEvent};
-
-use crate::{cmd::Cmd, gui::Model, run::session::S2EConfig, SessionConfig};
-
-mod session;
-mod run;
-mod control;
+pub mod session;
+pub mod run;
+pub mod control;
 pub mod embed;
