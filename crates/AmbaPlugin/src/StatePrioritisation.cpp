d020e7c052da74ac2e15e29f7e4583401d4c2835
Samuel Kyletoft <samuel@kyletoft.se>
2023-05-03 22:59:31 +0200

    AmbaPlugin/StatePrioritisation: Sleep to not block

--- crates/AmbaPlugin/src/StatePrioritisation.cpp
+++ crates/AmbaPlugin/src/StatePrioritisation.cpp
@@ -1,6 +1,9 @@
+#include <thread>
+#include <chrono>
 #include <vector>
 
 #include "StatePrioritisation.h"
+#include "Amba.h"
 #include "LibambaRs.h"
 
 namespace state_prioritisation {
@@ -9,9 +12,14 @@ namespace state_prioritisation {
 // join before the AmbaPlugin fields can be destructed
 void ipcReceiver(Ipc *ipc, bool *active, s2e::S2E *s2e) {
 	std::vector<u32> receive_buffer {};
+
 	while (*active) {
 		receive_buffer.clear();
 		bool received = rust_ipc_receive_message(ipc, &receive_buffer);
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		continue;
+
 		if (!received) {
 			continue;
 		}
